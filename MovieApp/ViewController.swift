82040fe51156821348fbea977829b61e6ff589c9
Mudassar Ahmad <mudassar.ahmad@starzplayarabia.com>
2022-12-08 00:03:58 +0500

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Mudassar Ahmad on 08/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
